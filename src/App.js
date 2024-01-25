fb29bee3ed424255f8572aa652b03ec731836b09
Daniel Rivas <drivas8891@gmail.com>
2024-01-24 16:03:06 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing build environment for react app
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/danieldotwav"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link to GitHub
         </a>
       </header>
     </div>
